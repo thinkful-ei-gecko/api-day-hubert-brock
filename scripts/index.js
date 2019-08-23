97aea4b487e020f35ad1003fe4de93a7eb8784b2
Hubert Yang <offspring7890@yahoo.com>
2019-08-23 13:51:50 -0700

    delete test code in index.js

--- scripts/index.js
+++ scripts/index.js
@@ -14,10 +14,6 @@ $(document).ready(function() {
   .then(res => res.json())
   .then((items) => {
     items.forEach((item) => store.addItem(item));
-    const item = store.items[0];
-    console.log('current name: ' + item.name);
-    store.findAndUpdate(item.id, { name: 'foobar' });
-    console.log('new name: ' + item.name);
     shoppingList.bindEventListeners();
     shoppingList.render();
   })
